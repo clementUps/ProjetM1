0fc991ea138d6e187a8e2d743189dfc5689ffe87
Lucas <lucas.bled12@gmail.com>
2015-03-17 10:28:54 +0100

    Premières modifications :  - debut d'implémentation des classes  - et ajout d'une classe test (pour voir la génération automatique)

--- app/src/main/java/com/projet/M1/action/Action.java
+++ app/src/main/java/com/projet/M1/action/Action.java
@@ -3,5 +3,6 @@ package com.projet.M1.action;
 /**
  * Created by clement on 15/03/2015.
  */
-public class Action {
+public abstract class Action {
+    private String nom;
 }
---
+++ app/src/main/java/com/projet/M1/enumerations/Conditions.java
@@ -0,0 +1,8 @@
+package com.projet.M1.enumerations;
+
+/**
+ * Created by Lucas on 17/03/2015.
+ */
+public enum Conditions {
+    IF_THEN, IF_THEN_ELSE; //...
+}
