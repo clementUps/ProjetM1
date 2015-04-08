b19f4827c7f3401ee4a7fa42398eb75e9468113e
ClementCa <clementcarretier@gmail.com>
2015-04-08 10:49:11 +0200

    modification de l'affichage rajout des listes ajout de configuration

--- app/src/main/java/servernode/example/com/projetm1/configurationEvenement.java
+++ app/src/main/java/servernode/example/com/projetm1/configurationEvenement.java
@@ -1,6 +1,5 @@
 package servernode.example.com.projetm1;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -16,50 +15,19 @@ import com.projet.M1.main.R;
  * Activities that contain this fragment must implement the
  * {@link configurationEvenement.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link configurationEvenement#newInstance} factory method to
+ * Use the {@link configurationEvenement} factory method to
  * create an instance of this fragment.
  */
 public class configurationEvenement extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment configurationEvenement.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static configurationEvenement newInstance(String param1, String param2) {
-        configurationEvenement fragment = new configurationEvenement();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public configurationEvenement() {
         // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -74,23 +42,6 @@ public class configurationEvenement extends Fragment {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
     }
 
     /**
